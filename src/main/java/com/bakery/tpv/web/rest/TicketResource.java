e77b19b3c00aaf51ad55f638e90e412d856e2072
Alvaro <alvarosanchezdam@gmail.com>
2017-04-04 11:14:47 +0200

    #11 añadir el valor recibido de la calculadora al precio del ticket

--- src/main/java/com/bakery/tpv/web/rest/TicketResource.java
+++ src/main/java/com/bakery/tpv/web/rest/TicketResource.java
@@ -125,7 +125,7 @@ public class TicketResource {
 
     @PutMapping("/tickets/{id}/calculadora/{valor}")
     @Timed
-    public ResponseEntity<Ticket> updateTicketAddOffer(@PathVariable long id, @PathVariable double valor) throws URISyntaxException {
+    public ResponseEntity<Ticket> updateTicketCalculator(@PathVariable long id, @PathVariable double valor) throws URISyntaxException {
         Ticket t = ticketRepository.findOne(id);
         t.setCantidad(t.getCantidad()+valor);
         Ticket result = ticketRepository.save(t);
